0ee5629328b7da5c8d95618d5fbada06d9132092
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2023-04-05 22:29:40 +0800

    Test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,9 @@ val buildCommit = providers.exec {
 }.standardOutput.asText.get().trim()
 
 val ciBuild = System.getenv("CI") == "true"
-val ciRef = System.getenv("GITHUB_HEAD_REF").orEmpty()
+val ciRef = System.getenv("GITHUB_REF").orEmpty()
 val ciRunNumber = System.getenv("GITHUB_RUN_NUMBER").orEmpty()
-val isReleaseBuild = ciBuild && ciRef == "main"
+val isReleaseBuild = ciBuild && ciRef.contains("main")
 val devReleaseName = if (ciBuild) "(Dev #$ciRunNumber)" else "($buildCommit)"
 
 val version = "2.0.0"
